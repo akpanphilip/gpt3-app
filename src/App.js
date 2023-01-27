6278205b6c204a54a0c97764500cd1b404f182d6
akpanphilip <akpanphilip1122@gmail.com>
2023-01-27 20:01:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import { Article, Navbar, Feature, CTA, Brand } from "./components";
 function App() {
   return (
     <div className="App">
