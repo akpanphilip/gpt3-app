f9dc97a446957efdfcf269f9bd90868b6c1e0572
akpanphilip <akpanphilip1122@gmail.com>
2023-01-31 03:26:19 +0100

    containers update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 import { CTA, Brand, Navbar } from "./components";
 import "./App.css";
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <div className="gradient__bg">
@@ -21,7 +21,6 @@ function App() {
       <Features />
       <Possibility />
       <CTA />
-      <Blog />
       <Blog />
       <Footer />
     </div>
